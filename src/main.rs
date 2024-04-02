fc7f7aa716e7fe238b228e492ff9a1f077b4ae1d
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-04-02 22:01:13 +0200

    feat: implement CLI
    Here we implement a simple CLI that receives a single argument in the
    form of a path to a file or stdin if the path is `-`. We create a reader
    from this.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello World!")
+use std::{
+    io::{stdin, Read},
+    process::ExitCode,
+};
+
+fn usage(prog: &str) -> String {
+    format!("Usage: {prog} [SOURCE]")
+}
+
+fn main() -> ExitCode {
+    let mut args = std::env::args();
+
+    let Some(prog) = args.next() else {
+        eprintln!("[ERROR]: No program name received through arguments");
+        return ExitCode::FAILURE;
+    };
+
+    let (Some(src), None) = (args.next(), args.next()) else {
+        eprintln!("[ERROR]: Invalid number of arguments provided, expected 1\n");
+        eprintln!("{}", usage(&prog));
+        return ExitCode::FAILURE;
+    };
+
+    let _src: Box<dyn Read> = match src.as_str() {
+        "-h" => {
+            println!("{}", usage(&prog));
+            return ExitCode::SUCCESS;
+        }
+        "--help" => {
+            println!("{}", usage(&prog));
+            return ExitCode::SUCCESS;
+        }
+        "help" => {
+            println!("{}", usage(&prog));
+            return ExitCode::SUCCESS;
+        }
+        "-" => Box::new(stdin().lock()),
+        path => Box::new(match std::fs::File::open(path) {
+            Ok(file) => file,
+            Err(err) => {
+                eprintln!("[ERROR]: failed to open file {path}: {err}");
+                return ExitCode::FAILURE;
+            }
+        }),
+    };
+
+    ExitCode::SUCCESS
 }
