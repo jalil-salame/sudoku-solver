0601a664d817b659c3d88235c94b476e87b84c4e
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-04-03 15:23:17 +0200

    feat: overhaul SudokuValue into SudokuCell
    Modify the Sudoku type to use SudokuCell, instead of SudokuValue.
    SudokuCell is a newtype around Option<SudokuValue> that allows us to
    pretty print cells properly and gives us more typesafety by explicitly
    setting the None value (instead of relying on non 1-9 values being
    equivalent to None).
    
    We also implement Index and IndexMut on the Sudoku allowing us to do
    `sudoku[(x, y)]` to access cells.

--- src/solver.rs
+++ src/solver.rs
@@ -1,19 +1,71 @@
+#![allow(dead_code)]
+use std::{
+    num::NonZeroU8,
+    ops::{Index, IndexMut},
+};
+
 #[repr(transparent)]
 #[derive(Debug, Clone, Copy)]
-struct SudokuValue(u8);
+pub struct SudokuValue(NonZeroU8);
+
+impl SudokuValue {
+    pub fn new(val: u8) -> Option<Self> {
+        (1..=9)
+            .contains(&val)
+            .then_some(SudokuValue(NonZeroU8::new(val)?))
+    }
+
+    pub unsafe fn new_unchecked(val: u8) -> Self {
+        SudokuValue(NonZeroU8::new_unchecked(val))
+    }
+
+    pub fn all() -> [SudokuValue; 9] {
+        [
+            unsafe { Self::new_unchecked(1) },
+            unsafe { Self::new_unchecked(2) },
+            unsafe { Self::new_unchecked(3) },
+            unsafe { Self::new_unchecked(4) },
+            unsafe { Self::new_unchecked(5) },
+            unsafe { Self::new_unchecked(6) },
+            unsafe { Self::new_unchecked(7) },
+            unsafe { Self::new_unchecked(8) },
+            unsafe { Self::new_unchecked(9) },
+        ]
+    }
+}
+
+#[repr(transparent)]
+#[derive(Default, Debug, Clone, Copy)]
+pub struct SudokuCell(Option<SudokuValue>);
+
+impl SudokuCell {
+    pub fn filled(val: SudokuValue) -> Self {
+        Self(Some(val))
+    }
+
+    pub fn empty() -> Self {
+        Self(None)
+    }
+
+    pub fn from_ascci_char(val: u8) -> Option<Self> {
+        if val == b'.' {
+            Some(Self::empty())
+        } else {
+            Some(Self::filled(SudokuValue::new(val.wrapping_sub(b'0'))?))
+        }
+    }
+}
 
 #[derive(Clone)]
-pub struct Sudoku([[SudokuValue; 9]; 9]);
+pub struct Sudoku([[SudokuCell; 9]; 9]);
 
 impl Sudoku {
     pub fn from_line(line: &[u8]) -> Self {
         assert_eq!(line.len(), 81);
-        let mut sudoku = [[SudokuValue(0); 9]; 9];
+        let mut sudoku = [[SudokuCell::empty(); 9]; 9];
         for (b, val) in line.iter().copied().zip(sudoku.iter_mut().flatten()) {
-            if (b'1'..=b'9').contains(&b) {
-                *val = SudokuValue(b - b'0')
-            } else if b == b'.' {
-                continue;
+            if let Some(v) = SudokuCell::from_ascci_char(b) {
+                *val = v;
             } else {
                 panic!(
                     "bad SudokuValue: b'{}' expected '.' or [1-9]",
@@ -25,201 +77,230 @@ impl Sudoku {
     }
 }
 
+impl Index<(usize, usize)> for Sudoku {
+    type Output = SudokuCell;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.0[y][x]
+    }
+}
+
+impl IndexMut<(usize, usize)> for Sudoku {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        &mut self.0[y][x]
+    }
+}
+
 impl std::fmt::Display for SudokuValue {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.0 {
+        match self.0.get() {
             digit @ 1..=9 => write!(f, "{digit}"),
-            _ => f.write_str("."),
+            val => {
+                unreachable!(
+                    "invalid SudokuValue: {val}, this should not be reachable through safe code"
+                )
+            }
+        }
+    }
+}
+
+impl std::fmt::Display for SudokuCell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(val) = self.0 {
+            write!(f, "{val}")
+        } else if f.alternate() {
+            write!(f, " ")
+        } else {
+            write!(f, ".")
         }
     }
 }
 
 impl std::fmt::Debug for Sudoku {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let vals = self.0;
         if f.alternate() {
             // Pretty print:
             write!(
                 f,
                 "+-------+-------+-------+
-| {} {} {} | {} {} {} | {} {} {} |
-| {} {} {} | {} {} {} | {} {} {} |
-| {} {} {} | {} {} {} | {} {} {} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
 +-------+-------+-------+
-| {} {} {} | {} {} {} | {} {} {} |
-| {} {} {} | {} {} {} | {} {} {} |
-| {} {} {} | {} {} {} | {} {} {} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
 +-------+-------+-------+
-| {} {} {} | {} {} {} | {} {} {} |
-| {} {} {} | {} {} {} | {} {} {} |
-| {} {} {} | {} {} {} | {} {} {} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
+| {:#} {:#} {:#} | {:#} {:#} {:#} | {:#} {:#} {:#} |
 +-------+-------+-------+",
-                vals[0][0],
-                vals[0][1],
-                vals[0][2],
-                vals[0][3],
-                vals[0][4],
-                vals[0][5],
-                vals[0][6],
-                vals[0][7],
-                vals[0][8],
-                vals[1][0],
-                vals[1][1],
-                vals[1][2],
-                vals[1][3],
-                vals[1][4],
-                vals[1][5],
-                vals[1][6],
-                vals[1][7],
-                vals[1][8],
-                vals[2][0],
-                vals[2][1],
-                vals[2][2],
-                vals[2][3],
-                vals[2][4],
-                vals[2][5],
-                vals[2][6],
-                vals[2][7],
-                vals[2][8],
-                vals[3][0],
-                vals[3][1],
-                vals[3][2],
-                vals[3][3],
-                vals[3][4],
-                vals[3][5],
-                vals[3][6],
-                vals[3][7],
-                vals[3][8],
-                vals[4][0],
-                vals[4][1],
-                vals[4][2],
-                vals[4][3],
-                vals[4][4],
-                vals[4][5],
-                vals[4][6],
-                vals[4][7],
-                vals[4][8],
-                vals[5][0],
-                vals[5][1],
-                vals[5][2],
-                vals[5][3],
-                vals[5][4],
-                vals[5][5],
-                vals[5][6],
-                vals[5][7],
-                vals[5][8],
-                vals[6][0],
-                vals[6][1],
-                vals[6][2],
-                vals[6][3],
-                vals[6][4],
-                vals[6][5],
-                vals[6][6],
-                vals[6][7],
-                vals[6][8],
-                vals[7][0],
-                vals[7][1],
-                vals[7][2],
-                vals[7][3],
-                vals[7][4],
-                vals[7][5],
-                vals[7][6],
-                vals[7][7],
-                vals[7][8],
-                vals[8][0],
-                vals[8][1],
-                vals[8][2],
-                vals[8][3],
-                vals[8][4],
-                vals[8][5],
-                vals[8][6],
-                vals[8][7],
-                vals[8][8],
+                self[(0, 0)],
+                self[(1, 0)],
+                self[(2, 0)],
+                self[(3, 0)],
+                self[(4, 0)],
+                self[(5, 0)],
+                self[(6, 0)],
+                self[(7, 0)],
+                self[(8, 0)],
+                self[(0, 1)],
+                self[(1, 1)],
+                self[(2, 1)],
+                self[(3, 1)],
+                self[(4, 1)],
+                self[(5, 1)],
+                self[(6, 1)],
+                self[(7, 1)],
+                self[(8, 1)],
+                self[(0, 2)],
+                self[(1, 2)],
+                self[(2, 2)],
+                self[(3, 2)],
+                self[(4, 2)],
+                self[(5, 2)],
+                self[(6, 2)],
+                self[(7, 2)],
+                self[(8, 2)],
+                self[(0, 3)],
+                self[(1, 3)],
+                self[(2, 3)],
+                self[(3, 3)],
+                self[(4, 3)],
+                self[(5, 3)],
+                self[(6, 3)],
+                self[(7, 3)],
+                self[(8, 3)],
+                self[(0, 4)],
+                self[(1, 4)],
+                self[(2, 4)],
+                self[(3, 4)],
+                self[(4, 4)],
+                self[(5, 4)],
+                self[(6, 4)],
+                self[(7, 4)],
+                self[(8, 4)],
+                self[(0, 5)],
+                self[(1, 5)],
+                self[(2, 5)],
+                self[(3, 5)],
+                self[(4, 5)],
+                self[(5, 5)],
+                self[(6, 5)],
+                self[(7, 5)],
+                self[(8, 5)],
+                self[(0, 6)],
+                self[(1, 6)],
+                self[(2, 6)],
+                self[(3, 6)],
+                self[(4, 6)],
+                self[(5, 6)],
+                self[(6, 6)],
+                self[(7, 6)],
+                self[(8, 6)],
+                self[(0, 7)],
+                self[(1, 7)],
+                self[(2, 7)],
+                self[(3, 7)],
+                self[(4, 7)],
+                self[(5, 7)],
+                self[(6, 7)],
+                self[(7, 7)],
+                self[(8, 7)],
+                self[(0, 8)],
+                self[(1, 8)],
+                self[(2, 8)],
+                self[(3, 8)],
+                self[(4, 8)],
+                self[(5, 8)],
+                self[(6, 8)],
+                self[(7, 8)],
+                self[(8, 8)],
             )
         } else {
             write!(
                 f, "{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}",
-                vals[0][0],
-                vals[0][1],
-                vals[0][2],
-                vals[0][3],
-                vals[0][4],
-                vals[0][5],
-                vals[0][6],
-                vals[0][7],
-                vals[0][8],
-                vals[1][0],
-                vals[1][1],
-                vals[1][2],
-                vals[1][3],
-                vals[1][4],
-                vals[1][5],
-                vals[1][6],
-                vals[1][7],
-                vals[1][8],
-                vals[2][0],
-                vals[2][1],
-                vals[2][2],
-                vals[2][3],
-                vals[2][4],
-                vals[2][5],
-                vals[2][6],
-                vals[2][7],
-                vals[2][8],
-                vals[3][0],
-                vals[3][1],
-                vals[3][2],
-                vals[3][3],
-                vals[3][4],
-                vals[3][5],
-                vals[3][6],
-                vals[3][7],
-                vals[3][8],
-                vals[4][0],
-                vals[4][1],
-                vals[4][2],
-                vals[4][3],
-                vals[4][4],
-                vals[4][5],
-                vals[4][6],
-                vals[4][7],
-                vals[4][8],
-                vals[5][0],
-                vals[5][1],
-                vals[5][2],
-                vals[5][3],
-                vals[5][4],
-                vals[5][5],
-                vals[5][6],
-                vals[5][7],
-                vals[5][8],
-                vals[6][0],
-                vals[6][1],
-                vals[6][2],
-                vals[6][3],
-                vals[6][4],
-                vals[6][5],
-                vals[6][6],
-                vals[6][7],
-                vals[6][8],
-                vals[7][0],
-                vals[7][1],
-                vals[7][2],
-                vals[7][3],
-                vals[7][4],
-                vals[7][5],
-                vals[7][6],
-                vals[7][7],
-                vals[7][8],
-                vals[8][0],
-                vals[8][1],
-                vals[8][2],
-                vals[8][3],
-                vals[8][4],
-                vals[8][5],
-                vals[8][6],
-                vals[8][7],
-                vals[8][8],
+                self[(0, 0)],
+                self[(1, 0)],
+                self[(2, 0)],
+                self[(3, 0)],
+                self[(4, 0)],
+                self[(5, 0)],
+                self[(6, 0)],
+                self[(7, 0)],
+                self[(8, 0)],
+                self[(0, 1)],
+                self[(1, 1)],
+                self[(2, 1)],
+                self[(3, 1)],
+                self[(4, 1)],
+                self[(5, 1)],
+                self[(6, 1)],
+                self[(7, 1)],
+                self[(8, 1)],
+                self[(0, 2)],
+                self[(1, 2)],
+                self[(2, 2)],
+                self[(3, 2)],
+                self[(4, 2)],
+                self[(5, 2)],
+                self[(6, 2)],
+                self[(7, 2)],
+                self[(8, 2)],
+                self[(0, 3)],
+                self[(1, 3)],
+                self[(2, 3)],
+                self[(3, 3)],
+                self[(4, 3)],
+                self[(5, 3)],
+                self[(6, 3)],
+                self[(7, 3)],
+                self[(8, 3)],
+                self[(0, 4)],
+                self[(1, 4)],
+                self[(2, 4)],
+                self[(3, 4)],
+                self[(4, 4)],
+                self[(5, 4)],
+                self[(6, 4)],
+                self[(7, 4)],
+                self[(8, 4)],
+                self[(0, 5)],
+                self[(1, 5)],
+                self[(2, 5)],
+                self[(3, 5)],
+                self[(4, 5)],
+                self[(5, 5)],
+                self[(6, 5)],
+                self[(7, 5)],
+                self[(8, 5)],
+                self[(0, 6)],
+                self[(1, 6)],
+                self[(2, 6)],
+                self[(3, 6)],
+                self[(4, 6)],
+                self[(5, 6)],
+                self[(6, 6)],
+                self[(7, 6)],
+                self[(8, 6)],
+                self[(0, 7)],
+                self[(1, 7)],
+                self[(2, 7)],
+                self[(3, 7)],
+                self[(4, 7)],
+                self[(5, 7)],
+                self[(6, 7)],
+                self[(7, 7)],
+                self[(8, 7)],
+                self[(0, 8)],
+                self[(1, 8)],
+                self[(2, 8)],
+                self[(3, 8)],
+                self[(4, 8)],
+                self[(5, 8)],
+                self[(6, 8)],
+                self[(7, 8)],
+                self[(8, 8)],
             )
         }
     }
