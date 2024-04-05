c39ca45cbafda9109630fede70becdc86b0fcf49
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-04-05 21:55:56 +0200

    feat: add iai-callgrind benchmarks
    We now have a dependency on iai-callgrind and added benchmarks for the
    iterative solver.

---
+++ benches/dfs-iai.rs
@@ -0,0 +1,20 @@
+use iai_callgrind::{library_benchmark, library_benchmark_group, main};
+use sudoku_solver::solver::IterativeDFS;
+use sudoku_solver::solver::Solver;
+use sudoku_solver::solver::Sudoku;
+
+const SUDOKU: &[u8; 81] =
+    b".......1.4.........2...........5.4.7..8...3....1.9....3..4..2...5.1........8.6...";
+
+#[library_benchmark]
+#[bench::first(Sudoku::from_line(SUDOKU))]
+fn solve_sudoku(sudoku: Sudoku) {
+    std::hint::black_box(IterativeDFS.solve(sudoku));
+}
+
+library_benchmark_group!(
+    name = solve_sudoku_group;
+    benchmarks = solve_sudoku,
+);
+
+main!(library_benchmark_groups = solve_sudoku_group);
